835c7c9966d3d9ba309163c6bf6244ee6deeb2d8
saira512dev <512saira@gmail.com>
2023-04-20 12:43:51 +0400

    set up api for searchSales

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,10 +1,43 @@
 import OverallStat from "../models/OverallStat.js";
+import axios from "axios"
 
 export const getSales = async(req, res) => {
     try {
-        const overallStats = await OverallStat.find();
+        // const overallStats = await OverallStat.find();
 
-        res.status(200).json(overallStats[0])
+        // res.status(200).json(overallStats[0])
+        res.status(200).json({"lll":"sss"})
+
+    } catch ( error ) {
+        res.status(404).json({message: error})
+    }
+}
+
+export const searchSales = async(req, res) => {
+    try {
+        // const overallStats = await OverallStat.find();
+        const { searchString } = req.query;
+        axios({
+            method: 'post',
+            url: 'https://api.dataforseo.com/v3/dataforseo_labs/google/historical_search_volume/live',
+            auth: {
+              username: 'login',
+              password: 'password'
+            },
+            data: JSON.parse(searchString),
+            headers: {
+              'content-type': 'application/json'
+            }
+          }).then(function (response) {
+            var result = response['data']['tasks'];
+            // Result data
+            console.log(result);
+          }).catch(function (error) {
+            console.log(error);
+          });
+        // console.log("REQUEST HERE")
+        // res.status(200).json(JSON.parse(searchString))
+
     } catch ( error ) {
         res.status(404).json({message: error})
     }
--- routes/sales.js
+++ routes/sales.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getSales } from "../controllers/sales.js"
+import { getSales, searchSales } from "../controllers/sales.js"
 import ensureAuth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/sales", ensureAuth, getSales)
+router.get("/searchSales", ensureAuth, searchSales)
 
 export default router;
