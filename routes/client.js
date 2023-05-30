3dbe1d2099043cd2b97e2b4036d377725148624d
saira512dev <512saira@gmail.com>
2023-05-30 20:24:57 +0530

    fixing products

--- routes/client.js
+++ routes/client.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getProducts, getCustomers, getTransactions, getGeography } from "../controllers/client.js";
+import { getProducts, getCustomers, getTransactions, getGeography,  } from "../controllers/client.js";
 import ensureAuth from "../middleware/auth.js";
 
 const router = express.Router();
@@ -8,6 +8,5 @@ router.get("/products", ensureAuth, getProducts);
 router.get("/customers", ensureAuth, getCustomers);
 router.get("/transactions", ensureAuth, getTransactions);
 router.get("/geography", ensureAuth, getGeography);
-
 
 export default router;
